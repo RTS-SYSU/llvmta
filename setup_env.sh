d0f856541e66257967edf54d979359a6840aba84
Zeng-WCh <lewis.zeng02@gmail.com>
2024-07-08 17:01:10 +0800

    <bug fixed>: fix setup_env

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export GUROBI_HOME=`pwd`/thirdParty/gurobi952/linux64
 export PATH=`pwd`/build/bin:${GUROBI_HOME}/bin:$PATH
-export LD_LIBRARY_PATH=${GUROBI_HOME}/lib":"${LD_LIBRARY_PATH}
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:${GUROBI_HOME}/include"
+export LD_LIBRARY_PATH=${GUROBI_HOME}/lib:${LD_LIBRARY_PATH}
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:${GUROBI_HOME}/include
 export GRB_LICENSE_FILE=${GUROBI_HOME}/gurobi.lic
