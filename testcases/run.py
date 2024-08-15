eccd35aa80b57a34bde25345664a12d6409f5ab0
Zeng-WCh <lewis.zeng02@gmail.com>
2024-08-15 18:49:36 +0800

    <feat>: Add generate LoopAnnotations

--- testcases/run.py
+++ testcases/run.py
@@ -32,6 +32,114 @@ args = parser.parse_args()
 logger_info(f'Using source directory: {args.src}')
 logger_info(f'Using output directory: {args.out}')
 
+def handle_generate(args):
+    src_dir = Path(os.path.abspath(args.src))
+    if not src_dir.exists():
+        logger_error(f'Source directory {src_dir} does not exist')
+        exit(1)
+
+    if not src_dir.is_dir():
+        logger_error(f'Source directory {src_dir} is not a directory')
+        exit(1)
+        
+    cf = Path(src_dir / args.core_info)
+    if not cf.exists():
+        logger_error(f'Core info file {str(cf)} does not exist, please check the file name')
+        exit(1)
+        
+    out_dir = Path(os.path.abspath(args.out))
+    if not out_dir.exists():
+        logger_info(f'Creating output directory {out_dir}')
+        out_dir.mkdir(parents=True)
+
+    if not out_dir.is_dir():
+        logger_error(f'Output directory {out_dir} is not a directory')
+        exit(1)
+
+    logger_success(f'All files exist, ready to run llvmta')
+
+    command = [
+        "llvmta",
+        "-disable-tail-calls",
+        "-float-abi=hard",
+        "-mattr=-neon,+vfp2",
+        "-O0",
+        "--ta-muarch-type=outoforder",
+        "--ta-memory-type=separatecaches",
+        "--ta-strict=false",
+        "--ta-num-callsite-tokens=1",
+        f"--core-info={str(cf)}",
+        f"--core-numbers={args.num_cores}",
+        "--shared-cache-Persistence-Analysis=false",
+        "--ta-output-unknown-loops",
+        "--ta-l2cache-persistence=elementwise",
+        "-debug-only=",
+        "optimized.ll"
+    ]
+
+    logger_info(f'Running llvmta with the following arguments:')
+    logger_info(f'Core info file: {str(cf)}')
+    logger_info(f'Number of cores: {args.num_cores}')
+
+
+    logger_info(f'Compiling the source file to LLVM IR')
+    # First, we need to compile the source file to LLVM IR
+    stat = os.system(f'./runBeforeGDB {str(src_dir)}')
+    if stat != 0:
+        logger_error(f'Failed to compile the source file to LLVM IR')
+        exit(1)
+    else:
+        logger_success(f'Successfully compiled the source file to LLVM IR')
+
+    logger_info(f'Running llvmta')
+    # Run llvmta
+    logger_info(f'Using command: {" ".join(command)}')
+    # Change working directory to the output directory
+
+    pwd = os.getcwd()
+    os.chdir('dirforgdb')
+    stat = os.system(' '.join(command))
+    if stat != 0:
+        logger_error(f'Failed to run llvmta')
+        exit(1)
+    else:
+        shutil.copy('LoopAnnotations.csv', out_dir / 'LoopAnnotations.csv')
+        shutil.copy('LoopAnnotations.csv', out_dir / 'LLoopAnnotations.csv')
+        
+        logger_success(f'Successfully ran llvmta')
+        logger_success(f'Output LoopAnnotations.csv: {str(out_dir / "LoopAnnotations.csv")}')
+        logger_success(f'Output LLoopAnnotations.csv: {str(out_dir / "LLoopAnnotations.csv")}')
+
+        with open(out_dir / 'LoopAnnotations.csv', 'r') as f:
+            data = f.readlines()
+            
+        output_data = list()
+            
+        for i in range(len(data)):
+            if i != 0 and data[i].startswith('#'):
+                continue
+            output_data.append(data[i])
+            
+        with open(out_dir / 'LoopAnnotations.csv', 'w') as f:
+            f.writelines(output_data)
+            
+        with open(out_dir / 'LLoopAnnotations.csv', 'r') as f:
+            data = f.readlines()
+        
+        output_data = list()
+        
+        for i in range(len(data)):
+            if i != 0 and data[i].startswith('#'):
+                continue
+            output_data.append(data[i])
+        
+        with open(out_dir / 'LLoopAnnotations.csv', 'w') as f:
+            f.writelines(output_data)
+            
+
+    os.chdir(pwd)
+
+def handle_run(args):
 src_dir = Path(os.path.abspath(args.src))
 if not src_dir.exists():
     logger_error(f'Source directory {src_dir} does not exist')
@@ -121,3 +229,9 @@ else:
     logger_success(f'Output WCET: {str(out_dir / "WCET.json")}')
 
 os.chdir(pwd)
+
+
+if args.p:
+    handle_generate(args)
+else:
+    handle_run(args)
